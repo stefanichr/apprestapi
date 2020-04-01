95c652e6571d62b18a84879b3a39710e20b6cf8d
stefanichr <stefanichristanti21@gmail.com>
2020-04-01 15:00:00 +0700

    berhasil menampilkan semua mahasiswa

--- koneksi.js
+++ koneksi.js
@@ -4,8 +4,8 @@ var mysql = require('mysql');
 const conn =  mysql.createConnection({
     host:'localhost',
     user:'root',
-    password:'',
-    database:'db_apprestapi ' 
+    password:'password',
+    database:'db_apprestapi' 
 });
 
 conn.connect((err)=>{
