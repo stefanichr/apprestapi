27d4f0d61a694a87d4128728784f6c301877b4b0
stefanichr <stefanichristanti21@gmail.com>
2020-04-01 14:14:13 +0700

    menambahkan kode res

---
+++ koneksi.js
@@ -0,0 +1,16 @@
+var mysql = require('mysql');
+
+//nuat koneksi database
+const conn =  mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'',
+    database:'db_apprestapi ' 
+});
+
+conn.connect((err)=>{
+    if(err) throw err;
+    console.log('Mysql terkoneksi');
+});
+
+module.exports = conn;
--- server.js
+++ server.js
@@ -9,4 +9,4 @@ app.use(bodyParser.json());
 
 app.listen(3000, () => {
     console.log(`Server started on port`);
-});
+});  
